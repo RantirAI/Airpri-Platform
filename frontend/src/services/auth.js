df060a48a72ac15a4820b58cc761c174d72a9863
Folareen <sakawahab03@gmail.com>
2023-07-07 15:41:07 +0100

    created auth services..

---
+++ frontend/src/services/auth.js
@@ -0,0 +1,47 @@
+import Axios, { setAxiosToken } from "../config/axios"
+import jwtDecode from "jwt-decode"
+import { authenticate } from "../redux/features/authSlice"
+
+export const signIn = async (data, dispatch) => {
+    try {
+        const response = await Axios.post('sign-in', data)
+        const token = response.data.token
+        const user = jwtDecode(token)
+        setAxiosToken(token)
+        dispatch(authenticate(user))
+        localStorage.removeItem('token')
+        localStorage.setItem('token', token)
+    } catch (error) {
+        throw new Error(error?.response?.data?.message || error.message)
+    }
+}
+
+export const createAccount = async (data, dispatch) => {
+    try {
+        const response = await Axios.post('create-account', data)
+        const token = response.data.token
+        const user = jwtDecode(token)
+        setAxiosToken(token)
+        dispatch(authenticate(user))
+        localStorage.removeItem('token')
+        localStorage.setItem('token', token)
+    } catch (error) {
+        throw new Error(error?.response?.data?.message || error.message)
+    }
+}
+
+export const requestPasswordReset = async (emailAddress) => {
+    try {
+        await Axios.patch('request-password-reset', {emailAddress})
+    } catch (error) {
+        throw new Error(error?.response?.data?.message || error.message)
+    }
+}
+
+export const resetPassword = async (data) => {
+    try {
+        await Axios.patch('reset-password', data)
+    } catch (error) {
+        throw new Error(error?.response?.data?.message || error.message)
+    }
+}

d56982053ab7d81526638346b05470cbcea76b3f
Folareen <sakawahab03@gmail.com>
2023-07-07 20:21:07 +0100

    created token model

---
+++ backend/src/models/Token.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose')
+
+const TokenSchema = new Schema({
+    token: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }, purpose: {
+        type: String,
+        required: true
+    }, createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 3600,
+    }, status: {
+        type: Boolean,
+        required: false,
+        default: false
+    }
+})
+
+const Token = model('Token', TokenSchema)
+
+module.exports = Token

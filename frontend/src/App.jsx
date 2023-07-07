c3312ab32c63bff7c2a36197c0a2713b7d420bdb
Folareen <sakawahab03@gmail.com>
2023-07-07 15:42:18 +0100

    darktheme and toast setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import { Provider } from 'react-redux'
 import Routes from './Routes'
 import store from './redux/store'
+import { DarkThemeToggle, Flowbite } from 'flowbite-react';
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Routes />
+      <Flowbite>
+        <DarkThemeToggle className='fixed top-2 right-2' />
+        <Routes />
+      </Flowbite>
+      <ToastContainer />
     </Provider>
   );
 }

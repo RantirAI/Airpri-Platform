f0773d243211e29e42d1c1dd9072fadce1394e5b
Folareen <sakawahab03@gmail.com>
2023-07-13 23:04:51 +0100

    added dark mode toggle button to auth pages

--- frontend/src/components/layouts/AuthContainer.jsx
+++ frontend/src/components/layouts/AuthContainer.jsx
@@ -3,10 +3,12 @@ import authImage from '../../assets/auth-image.png'
 import authDarkModeLogo from '../../assets/auth-dark-mode-logo.svg'
 import authLightModeLogo from '../../assets/auth-light-mode-logo.svg'
 import { Link } from 'react-router-dom'
+import { DarkThemeToggle } from 'flowbite-react'
 
 const AuthContainer = ({ children, title }) => {
   return (
     <div className='flex justify-center items-center min-h-screen bg-gray-300 dark:bg-[#101016] font-inter'>
+      <DarkThemeToggle className='fixed top-2 right-2' />
       <div className='w-5/6 md:w-4/5 lg:w-2/3 max-w-5xl flex rounded-[16px] bg-gray-50 dark:bg-gray-600'>
         <div className='w-[35%] hidden lg:block rounded-tl-[16px] rounded-bl-[16px] min-h-full'>
           <img src={authImage} className='rounded-tl-[16px] rounded-bl-[16px] h-full w-full object-cover' />

447778bb653cb63c4f2c3254751319c4896dfde5
Folareen <sakawahab03@gmail.com>
2023-07-15 21:28:31 +0100

    done with footer

--- frontend/src/components/layouts/MainContainer/Footer.jsx
+++ frontend/src/components/layouts/MainContainer/Footer.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { FaFacebookF, FaGithub, FaGlobe, FaTwitter } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='w-full mt-auto py-[48px] px-[12px] bg-white dark:bg-[#111928] rounded-[8px] '>
+      <div className='flex justify-between items-center text-gray-500 dark:text-white'>
+        <p className='text-base font-normal'>
+          © 2023 Rantir, Inc. All rights reserved.
+        </p>
+        <div className='flex gap-[20px] '>
+          <FaFacebookF />
+          <FaTwitter />
+          <FaGithub />
+          <FaGlobe />
+        </div>
+      </div>
+    </footer>
   )
 }
 

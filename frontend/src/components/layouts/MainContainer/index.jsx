a00c8a540d85af7d826988c9bb69c456542e1ead
Folareen <sakawahab03@gmail.com>
2023-07-16 23:45:58 +0100

    done with main container

--- frontend/src/components/layouts/MainContainer/index.jsx
+++ frontend/src/components/layouts/MainContainer/index.jsx
@@ -5,13 +5,15 @@ import LeftPane from './LeftPane'
 
 const MainContainer = ({ children }) => {
     return (
-        <div>
+        <div className=' flex flex-col bg-gray-300 dark:bg-[#101016] '>
             <Header />
-            <div>
                 <LeftPane />
-                {children}
-            </div>
-            <Footer />
+                <div style={{left: 'calc(100% / 6)' }} className='min-h-screen w-5/6 relative pt-16 flex flex-col px-[20px] py-[10px]'>
+                    <div >
+                    {children}
+                    </div>
+                    <Footer/>
+                </div>
         </div>
     )
 }

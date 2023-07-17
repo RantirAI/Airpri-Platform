b0eec2cb2b44dca1f352e9511f87a81af9b4ced8
Folareen <sakawahab03@gmail.com>
2023-07-17 23:47:32 +0100

    added modals to main container

--- frontend/src/components/layouts/MainContainer/index.jsx
+++ frontend/src/components/layouts/MainContainer/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import DeleteWorkspaceModal from '../../modals/DeleteWorkspaceModal'
+import NewWorkspaceModal from '../../modals/NewWorkspaceModal'
 import Footer from './Footer'
 import Header from './Header'
 import LeftPane from './LeftPane'
@@ -7,13 +9,16 @@ const MainContainer = ({ children }) => {
     return (
         <div className=' flex flex-col bg-gray-300 dark:bg-[#101016] '>
             <Header />
-                <LeftPane />
-                <div style={{left: 'calc(100% / 6)' }} className='min-h-screen w-5/6 relative pt-16 flex flex-col px-[20px] py-[10px]'>
-                    <div >
+            <LeftPane />
+            <div style={{ left: 'calc(100% / 6)' }} className='min-h-screen w-5/6 relative pt-16 flex flex-col px-[20px] py-[10px]'>
+                <div >
                     {children}
-                    </div>
-                    <Footer/>
                 </div>
+                <Footer />
+
+                <NewWorkspaceModal />
+                <DeleteWorkspaceModal />
+            </div>
         </div>
     )
 }

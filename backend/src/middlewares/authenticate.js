9ee75f7a15e4ffa5ec5d879c233e9f4a29b528a3
Folareen <sakawahab03@gmail.com>
2023-07-17 17:44:17 +0100

    removed jargons from response message

--- backend/src/middlewares/authenticate.js
+++ backend/src/middlewares/authenticate.js
@@ -21,7 +21,7 @@ const authenticate = async (req, res, next) => {
         const user = await User.findOne({ _id: decodedUser._id })
         
         if(!user){
-            return res.status(401).json({message: "Unauthorized fdfsdffds"})
+            return res.status(401).json({message: "Unauthorized"})
         }
 
         req.user = user

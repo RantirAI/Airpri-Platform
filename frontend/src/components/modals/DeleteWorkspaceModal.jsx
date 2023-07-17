99ad037b7b4c58ca46c6fdcddb020f26d5590019
Folareen <sakawahab03@gmail.com>
2023-07-17 23:48:18 +0100

    created delete workspace modal

---
+++ frontend/src/components/modals/DeleteWorkspaceModal.jsx
@@ -0,0 +1,70 @@
+import { Button, Modal, Spinner } from 'flowbite-react';
+import { useState } from 'react';
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { toggleDeleteWorkspaceModal } from '../../redux/features/modalsSlice';
+import { unselectWorkspace } from '../../redux/features/workspaceSlice';
+import { deleteWorkspace } from '../../services/workspace';
+
+const DeleteWorkspaceModal = () => {
+    const { showDeleteWorkspaceModal } = useSelector(state => state.modals)
+    const { workspace } = useSelector(state => state.workspace)
+
+    const [submitting, setSubmitting] = useState(false)
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleDelete = async (e) => {
+        try {
+            e.preventDefault()
+            setSubmitting(true)
+            await deleteWorkspace(workspace._id)
+            toast.info('Workspace deleted!')
+            dispatch(toggleDeleteWorkspaceModal(false))
+            dispatch(unselectWorkspace())
+            navigate('/dashboard')
+        } catch (error) {
+            toast.error(error.message)
+        } finally {
+            setSubmitting(false)
+        }
+    }
+
+    return (
+        <Modal dismissible show={showDeleteWorkspaceModal} size="md" popup onClose={() => dispatch(toggleDeleteWorkspaceModal(false))}>
+            <Modal.Header />
+            <Modal.Body>
+                <div className="text-center">
+                    <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                        Are you sure you want to delete this workspace?
+                    </h3>
+                    <div className="flex justify-center gap-4">
+                        <Button color="failure" onClick={handleDelete}>
+                            {
+                                submitting ?
+                                    <>
+                                        <Spinner aria-label="Deleting workspace" />
+                                        <span className="pl-3">
+                                            Loading...
+                                        </span>
+                                    </>
+                                    :
+                                    "Yes, I'm sure"
+                            }
+                        </Button>
+                        <Button color="gray" onClick={() => dispatch(toggleDeleteWorkspaceModal(false))}>
+                            No, cancel
+                        </Button>
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal>
+    )
+}
+
+
+export default DeleteWorkspaceModal

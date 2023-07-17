4edb911715606a8fe8752b812d558c8dea8e1a34
Folareen <sakawahab03@gmail.com>
2023-07-17 13:04:33 +0100

    added delete workspace to route

--- backend/src/routes/workspace.js
+++ backend/src/routes/workspace.js
@@ -1,11 +1,11 @@
 const {Router} = require('express')
-const { createWorkspace, archiveWorkspace, getWorkspaces } = require('../controllers/workspace')
+const { createWorkspace, archiveWorkspace, getWorkspaces, deleteWorkspace } = require('../controllers/workspace')
 const validateObjectId = require('../middlewares/validateObjectId')
 
 const router = Router()
 
 router.route('/').post(createWorkspace).get(getWorkspaces)
 router.param("id", validateObjectId)
-router.route('/:id').patch(archiveWorkspace)
+router.route('/:id').patch(archiveWorkspace).delete(deleteWorkspace)
 
 module.exports = router

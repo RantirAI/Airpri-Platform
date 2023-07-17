3a42664f7a95e26542086942d94216d314bd7790
Folareen <sakawahab03@gmail.com>
2023-07-18 00:30:06 +0100

    used workspace updatedAt value as item time

--- frontend/src/pages/main/Dashboard.jsx
+++ frontend/src/pages/main/Dashboard.jsx
@@ -18,7 +18,7 @@ const Dashboard = () => {
 
   const { workspace } = useSelector(state => state.workspace)
 
-  const {showNewWorkspaceModal} = useSelector(state => state.modals)
+  const { showNewWorkspaceModal } = useSelector(state => state.modals)
 
   const workspaces = useFetch('workspace', [showNewWorkspaceModal])
 
@@ -92,7 +92,7 @@ const Dashboard = () => {
 
                 <div>
                   {
-                    workspaces?.data?.workspaces?.map(({ name, spreadsheets, forms, gallery }) => (
+                    workspaces?.data?.workspaces?.map(({ name, spreadsheets, forms, gallery, updatedAt }) => (
                       <div>
                         <div className='mb-[12px] flex items-center gap-[16px]'>
                           <img src={rectangleStackImg} className='w-[32px] h-[32px] ' />
@@ -105,9 +105,9 @@ const Dashboard = () => {
                         </p>
 
                         <div className='py-[24px] gap-[24px] flex flex-row flex-wrap'>
-                          <WorkspaceItemCard type={'spreadsheets'} time={'spreadsheets.time'} />
-                          <WorkspaceItemCard type={'forms'} time={'forms.time'} />
-                          <WorkspaceItemCard type={'gallery'} time={'gallery.time'} />
+                          <WorkspaceItemCard type={'spreadsheets'} time={updatedAt} />
+                          <WorkspaceItemCard type={'forms'} time={updatedAt} />
+                          <WorkspaceItemCard type={'gallery'} time={updatedAt} />
 
                         </div>
 

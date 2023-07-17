8348aa45e3d2fd256d61ed95250e46d829ca4806
Folareen <sakawahab03@gmail.com>
2023-07-17 12:31:17 +0100

    added archiveWorkspace  & id validation middleware

--- backend/src/routes/workspace.js
+++ backend/src/routes/workspace.js
@@ -1,8 +1,11 @@
 const {Router} = require('express')
-const { createWorkspace } = require('../controllers/workspace')
+const { createWorkspace, archiveWorkspace } = require('../controllers/workspace')
+const validateObjectId = require('../middlewares/validateObjectId')
 
 const router = Router()
 
 router.route('/').post(createWorkspace)
+router.param("id", validateObjectId)
+router.route('/:id').patch(archiveWorkspace)
 
 module.exports = router

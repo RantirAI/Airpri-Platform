7300245d816ffa58857dfe76949e12f90598977e
Folareen <sakawahab03@gmail.com>
2023-07-17 11:44:36 +0100

    added workspace route

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ const connectToDB = require('./src/utils/connectToDB')
 require('dotenv').config()
 const express = require('express')
 const authRouter = require('./src/routes/auth')
+const workspaceRouter = require('./src/routes/workspace')
 const cors = require('cors')
 const notFound = require('./src/middlewares/notFound')
+const authenticate = require('./src/middlewares/authenticate')
 
 const app = express()
 
@@ -12,6 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/api/v1', authRouter)
+app.use('/api/v1/workspace', authenticate, workspaceRouter)
 
 app.use(notFound)
 

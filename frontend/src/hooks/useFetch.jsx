e65b3e45a16f16a829bf7fdecdda7226e7c873f6
Folareen <sakawahab03@gmail.com>
2023-07-17 21:04:29 +0100

    created use fetch hook

---
+++ frontend/src/hooks/useFetch.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import Axios from '../config/axios'
+
+const useFetch = (url, dependencies) => {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+
+    useEffect(() => {
+        (
+            async () => {
+                try {
+                    setLoading(true)
+                    const response = await Axios.get(url)
+                    setData(response.data)
+                } catch (error) {
+                    setError(error.message)
+                } finally {
+                    setLoading(false)
+                }
+            }
+        )()
+    }, [...dependencies])
+
+    return {
+        data,
+        loading,
+        error
+    }
+}
+
+export default useFetch

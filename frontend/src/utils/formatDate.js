cbdb3d968712990760d9e99cf53f59da34bf6a02
Folareen <sakawahab03@gmail.com>
2023-07-17 21:06:01 +0100

    created format date func

---
+++ frontend/src/utils/formatDate.js
@@ -0,0 +1,46 @@
+export const months = [
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december"
+]
+
+
+export const getDateAndTime = (timestamp) => {
+    const date = new Date(`${timestamp}`);
+
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+
+
+    const time = new Date(`${timestamp}`).toLocaleTimeString()
+
+
+    if (month && day && year && time) {
+        return `${month} ${day}, ${year}. ${time.substring(-2, time.length - 3)}`;
+    }
+
+    return 'N/A'
+};
+
+export const getDate = (timestamp) => {
+    const date = new Date(`${timestamp}`);
+
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+
+    if (month && day && year)
+        return `${day} ${month} ${year}`;
+
+    return 'N/A'
+};

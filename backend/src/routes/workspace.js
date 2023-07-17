13a9c294bd27d0aab54f4a3452c0589fe5f4f5de
Folareen <sakawahab03@gmail.com>
2023-07-17 12:51:24 +0100

    added getworkspaces controller to route

--- backend/src/routes/workspace.js
+++ backend/src/routes/workspace.js
@@ -1,10 +1,10 @@
 const {Router} = require('express')
-const { createWorkspace, archiveWorkspace } = require('../controllers/workspace')
+const { createWorkspace, archiveWorkspace, getWorkspaces } = require('../controllers/workspace')
 const validateObjectId = require('../middlewares/validateObjectId')
 
 const router = Router()
 
-router.route('/').post(createWorkspace)
+router.route('/').post(createWorkspace).get(getWorkspaces)
 router.param("id", validateObjectId)
 router.route('/:id').patch(archiveWorkspace)
 

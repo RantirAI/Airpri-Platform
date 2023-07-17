067e4105e29c95bc6a5d15d8653e0d633ad707b2
Folareen <sakawahab03@gmail.com>
2023-07-17 13:04:05 +0100

    created delete workspace

--- backend/src/controllers/workspace.js
+++ backend/src/controllers/workspace.js
@@ -62,8 +62,29 @@ const getWorkspaces = async (req, res) => {
     }
 }
 
+const deleteWorkspace = async (req, res) => {
+    try{
+        const {id} =  req.params
+
+        const workspace = await Workspace.findById(id)
+
+        if(!workspace){
+            return res.status(404).json({message: 'Workspace not found'})
+        }
+
+        await workspace.deleteOne()
+
+        res.status(200).json({message: 'Workspace deleted successfully'})
+    } catch(error) {
+        console.log(error.message)
+        res.status(500).json({message: 'Something went wrong'})
+    }
+}
+
+
 module.exports = {
     createWorkspace,
     archiveWorkspace,
     getWorkspaces,
+    deleteWorkspace
 }

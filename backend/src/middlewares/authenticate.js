0956c87d314c1040cdf14516e6c9f16aa22fe725
Folareen <sakawahab03@gmail.com>
2023-07-17 11:43:41 +0100

    added verify token middleware

---
+++ backend/src/middlewares/authenticate.js
@@ -0,0 +1,39 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+const authenticate = async (req, res, next) => {
+    try {
+        if (!req.headers?.authorization) {
+            return res.status(401).json({ message: 'Unauthorized' })
+        }
+
+        const token = req.headers?.authorization?.split(' ')[1]
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized' })
+        }
+
+        const decodedUser = jwt.decode(token, process.env.JWT_SECRET)
+
+        if (!decodedUser) {
+            return res.status(401).json({ message: 'Unauthorized' })
+        }
+
+        console.log(decodedUser)
+
+        const user = await User.findOne({ _id: decodedUser._id })
+        
+        if(!user){
+            return res.status(401).json({message: "Unauthorized fdfsdffds"})
+        }
+
+        req.user = user
+
+        next()
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ message: 'Unable to authenticate user' })
+    }
+
+}
+
+module.exports = authenticate

349c672c94c3a1fb375de4f90dace58cc89f8a77
Folareen <sakawahab03@gmail.com>
2023-07-17 10:55:07 +0100

    demo data

---
+++ frontend/data.js
@@ -0,0 +1,14 @@
+export const workspaces = [
+    {
+        name: 'Drivers',
+        spreadsheets: {
+            time: 'July 2, 2023 6:41 PM'
+        },
+        forms: {
+            time: 'July 2, 2023 6:41 PM'
+        },
+        gallery: {
+            time: 'July 2, 2023 6:41 PM'
+        }
+    }
+]

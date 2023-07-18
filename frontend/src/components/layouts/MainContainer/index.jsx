955bb023f7126ff75c0539982f14bb60d2285893
Folareen <sakawahab03@gmail.com>
2023-07-18 17:00:44 +0100

    changed light mode background color

--- frontend/src/components/layouts/MainContainer/index.jsx
+++ frontend/src/components/layouts/MainContainer/index.jsx
@@ -11,7 +11,7 @@ import LeftPane from './LeftPane'
 const MainContainer = ({ children }) => {
     const [showLeftPane, setShowLeftPane] = useState(false)
     return (
-        <div className=' flex flex-col bg-gray-300 dark:bg-[#101016] '>
+        <div className=' flex flex-col bg-[#FDFDFD] dark:bg-[#101016] '>
             <Header showLeftPane={showLeftPane} setShowLeftPane={setShowLeftPane} />
             <LeftPane showLeftPane={showLeftPane} setShowLeftPane={setShowLeftPane} />
             <div className='min-h-screen w-full lg:w-4/5 left-0 lg:left-[20%] relative pt-16 flex flex-col px-[10px] lg:px-[20px] py-[10px]'>

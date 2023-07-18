1a14f4deb294b74e7a6dbcdc5df0bd7aa9762f8a
Folareen <sakawahab03@gmail.com>
2023-07-18 02:06:06 +0100

    created edit workspace controller

--- backend/src/controllers/workspace.js
+++ backend/src/controllers/workspace.js
@@ -28,67 +28,106 @@ const createWorkspace = async (req, res) => {
     }
 }
 
-const archiveWorkspace = async (req, res) => {
-    try{
-        const {id} =  req.params
-                
+const editWorkspace = async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, description, members } = req.body
+
         const workspace = await Workspace.findById(id)
 
-        if(!workspace){
-            return res.status(404).json({message: 'Workspace not found'})
+        if (name) {
+            const nameExists = await Workspace.findOne({ name })
+            if (nameExists) {
+                if (nameExists._id != id) {
+                    return res.status(400).json({ message: "Name is taken" })
+                } else {
+                    workspace.name = name
+                }
+            } else {
+                workspace.name = name
+            }
         }
 
-        if(workspace.archived){
-            return res.status(400).json({message: 'Workspace already archived'})
+        if (description) {
+            workspace.description = description
+        }
+
+        if (members) {
+            workspace.members = !(members.includes(String(req.user._id))) ? [...members, req.user._id] : members
+        }
+
+        await workspace.save()
+
+        res.status(200).json({ message: 'Workspace updated', workspace })
+    }
+    catch (error) {
+        console.log(error.message)
+        res.status(500).json({ message: 'Something went wrong' })
+    }
+}
+
+const archiveWorkspace = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const workspace = await Workspace.findById(id)
+
+        if (!workspace) {
+            return res.status(404).json({ message: 'Workspace not found' })
+        }
+
+        if (workspace.archived) {
+            return res.status(400).json({ message: 'Workspace already archived' })
         }
 
         workspace.archived = true
 
         await workspace.save()
 
-        res.status(200).json({message: 'Workspace archived successfully'})
+        res.status(200).json({ message: 'Workspace archived successfully' })
 
-    } catch(error) {
+    } catch (error) {
         console.log(error.message)
-        res.status(500).json({message: 'Something went wrong'})
+        res.status(500).json({ message: 'Something went wrong' })
     }
 }
 
 const getWorkspaces = async (req, res) => {
-    try{
-        const {archived} = req.query
-        const workspaces = await Workspace.find({orgName: req.user.orgName, members: { $in: [req.user._id] }, archived: archived ? true: false}).populate('members')
-        
-        res.status(200).json({workspaces})
+    try {
+        const { archived } = req.query
+        const workspaces = await Workspace.find({ orgName: req.user.orgName, members: { $in: [req.user._id] }, archived: archived ? true : false }).populate('members')
 
-    } catch(error) {
+        res.status(200).json({ workspaces })
+
+    } catch (error) {
         console.log(error.message)
-        res.status(500).json({message: 'Something went wrong'})
+        res.status(500).json({ message: 'Something went wrong' })
     }
 }
 
 const deleteWorkspace = async (req, res) => {
-    try{
-        const {id} =  req.params
+    try {
+        const { id } = req.params
 
         const workspace = await Workspace.findById(id)
 
-        if(!workspace){
-            return res.status(404).json({message: 'Workspace not found'})
+        if (!workspace) {
+            return res.status(404).json({ message: 'Workspace not found' })
         }
 
         await workspace.deleteOne()
 
-        res.status(200).json({message: 'Workspace deleted successfully'})
-    } catch(error) {
+        res.status(200).json({ message: 'Workspace deleted successfully' })
+    } catch (error) {
         console.log(error.message)
-        res.status(500).json({message: 'Something went wrong'})
+        res.status(500).json({ message: 'Something went wrong' })
     }
 }
 
 
 module.exports = {
     createWorkspace,
+    editWorkspace,
     archiveWorkspace,
     getWorkspaces,
     deleteWorkspace

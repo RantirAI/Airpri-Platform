4eecdca53929c776ac82b13bbb1ffa70cac4239a
Folareen <sakawahab03@gmail.com>
2023-07-21 13:30:11 +0100

    done with get spreadsheet

--- backend/src/controllers/spreadsheet.js
+++ backend/src/controllers/spreadsheet.js
@@ -43,7 +43,34 @@ const createSpreadsheet = async (req, res) => {
     }
 }
 
+const getSpreadsheet = async (req, res) => {
+    try {
+        const id = req.params.id
+        const spreadsheet = await Spreadsheet.findById(id).populate('workspace')
+
+        if (!spreadsheet) {
+            return res.status(404).json({ message: 'Spreadsheet not found' })
+        }
+
+        const workspace = await Workspace.findById(spreadsheet.workspace)
+
+        const userInWorkspace = workspace.members.find((member) => (
+            String(member) == String(req.user._id)
+        ))
+
+        if (!userInWorkspace) {
+            return res.status(404).json({ message: 'Spreadsheet not found' })
+        }
+
+        return res.status(200).json({ spreadsheet })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ message: 'Something went wrong' })
+    }
+}
+
 
 module.exports = {
-    createSpreadsheet
+    createSpreadsheet,
+    getSpreadsheet
 }
--- backend/src/routes/spreadsheet.js
+++ backend/src/routes/spreadsheet.js
@@ -1,8 +1,11 @@
 const {Router} = require('express')
-const { createSpreadsheet } = require('../controllers/spreadsheet')
+const { createSpreadsheet, getSpreadsheet } = require('../controllers/spreadsheet')
+const validateObjectId = require('../middlewares/validateObjectId')
 
 const router = Router()
 
 router.route('/').post(createSpreadsheet)
+router.param("id", validateObjectId)
+router.route('/:id').get(getSpreadsheet)
 
 module.exports = router

fffcf51b8783fd4ac06047c3f24045ffc1844a65
Folareen <sakawahab03@gmail.com>
2023-07-21 16:28:44 +0100

    created spreadsheet access authorization middleware

---
+++ backend/src/middlewares/authorizeSpreadsheetAccess.js
@@ -0,0 +1,31 @@
+const Spreadsheet = require("../models/Spreadsheet")
+const Workspace = require("../models/Workspace")
+
+const authorizeSpreadsheetAccess = async (req, res, next) => {
+    try {
+        const id = req.params.id
+        const spreadsheet = await Spreadsheet.findById(id).populate('workspace')
+
+        if (!spreadsheet) {
+            return res.status(404).json({ message: 'Spreadsheet not found' })
+        }
+
+        const workspace = await Workspace.findById(spreadsheet.workspace)
+
+        const userInWorkspace = workspace?.members.find((member) => (
+            String(member) == String(req.user._id)
+        ))
+
+        if (!userInWorkspace) {
+            return res.status(404).json({ message: 'Spreadsheet not found' })
+        }
+
+        next()
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ message: 'Something went wrong' })
+    }
+}
+
+module.exports = authorizeSpreadsheetAccess

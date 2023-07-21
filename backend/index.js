78e80a0072a28bae8583a6d36dc1cacbb0353308
Folareen <sakawahab03@gmail.com>
2023-07-21 12:00:17 +0100

    added spreadsheet route

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const authRouter = require('./src/routes/auth')
 const workspaceRouter = require('./src/routes/workspace')
 const usersRouter = require('./src/routes/user')
+const spreadsheetRouter = require('./src/routes/spreadsheet')
 const cors = require('cors')
 const notFound = require('./src/middlewares/notFound')
 const authenticate = require('./src/middlewares/authenticate')
@@ -12,25 +13,27 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1', authRouter)
-app.use('/api/v1/workspace', authenticate, workspaceRouter)
+app.use(authenticate)
+app.use('/api/v1/workspace' , workspaceRouter)
 app.use('/api/v1', usersRouter)
+app.use('/api/v1/spreadsheet', spreadsheetRouter)
 
 app.use(notFound)
 
 const PORT = process.env.PORT || 5000
 
 const startServer = async () => {
-        try {
-            await connectToDB()
-            app.listen(PORT, () => {
-                console.log(`Server started at port ${PORT}`)
-            })
-        } catch (error) {
-            console.log(error.message)
-        }
+    try {
+        await connectToDB()
+        app.listen(PORT, () => {
+            console.log(`Server started at port ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error.message)
+    }
 }
 
 startServer()

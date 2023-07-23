1fe92a4c5db8faccf68b274927704cc75280b2ed
Folareen <sakawahab03@gmail.com>
2023-07-23 23:54:41 +0100

    created spreadsheets page..

---
+++ frontend/src/pages/main/Spreadsheets.jsx
@@ -0,0 +1,108 @@
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import MainContainer from '../../components/layouts/MainContainer'
+import { Alert, Button, TextInput } from 'flowbite-react'
+import welcomeIllustration from '../../assets/welcome-illustration.svg'
+import { HiDownload, HiOutlinePlusCircle } from 'react-icons/hi'
+import WorkspaceItemCard from '../../components/cards/WorkspaceItemCard'
+import { FiSettings } from 'react-icons/fi'
+import { SlOptionsVertical } from 'react-icons/sl'
+import rectangleStackImg from '../../assets/rectangle-stack.svg'
+import workspaceImage from '../../assets/image-workspace.svg'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { getDateAndTime } from '../../utils/formatDate'
+import { toggleArchiveWorkspaceModal, toggleDeleteWorkspaceModal, toggleDuplicateWorkspaceModal, toggleWorkspaceSettingsModal } from '../../redux/features/modalsSlice'
+import SpreadsheetCard from '../../components/cards/SpreadsheetCard'
+
+const Spreadsheets = () => {
+
+    const [showOptions, setShowOptions] = useState(false)
+    const { workspace } = useSelector(state => state.workspace)
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    return (
+        <MainContainer>
+
+            <div className='p-[15px] bg-white dark:bg-[#111928] flex flex-wrap gap-2 justify-between  items-center mt-[15px] rounded-[8px]'>
+                <div className='flex gap-[15px] items-center min-w-max'>
+                    <TextInput
+                        id='workspace-search'
+                        placeholder='Search inside the entire Workspace'
+                        required
+                        type='search'
+                        className='min-w-[250px] lg:min-w-[500px] flex-1'
+                    />
+                    <button className='border-none outline-none text-gray-900 dark:text-white' onClick={() => {
+                        dispatch(toggleWorkspaceSettingsModal(true))
+                    }}>
+                        <FiSettings />
+                    </button>
+                    <div className='relative min-w-max text-gray-900 dark:text-white'>
+                        <button className='w-full min-w-[222px] ' onClick={() => {
+                            setShowOptions(!showOptions)
+                        }}>
+                            <SlOptionsVertical />
+                        </button>
+                        {
+                            showOptions &&
+                            <div className='absolute w-full bg-white dark:bg-gray-700 shadow-sm rounded-[6px]' onClick={() => {
+                                setShowOptions(false)
+                            }}>
+                                <button className=' text-gray-700 dark:text-white w-full text-sm font-normal py-[8px] px-[16px] text-left ' onClick={() => {
+                                    dispatch(toggleDuplicateWorkspaceModal(true))
+                                }}>
+                                    Duplicate Workspace
+                                </button>
+                                <button className=' text-gray-700 dark:text-white w-full text-sm font-normal py-[8px] px-[16px] text-left' onClick={() => {
+                                    dispatch(toggleArchiveWorkspaceModal(true))
+                                }}>
+                                    Archive Workspace
+                                </button>
+                                <button className='border-gray-200 border-0 border-solid border-t-[1px] text-red-500 w-full text-sm font-normal py-[8px] px-[16px] text-left' onClick={() => {
+                                    dispatch(toggleDeleteWorkspaceModal(true))
+                                }}>
+                                    Delete Workspace (Forever)
+                                </button>
+                            </div>
+                        }
+
+                    </div>
+                </div>
+                <div className='gap-[10px] flex flex-row flex-wrap items-center '>
+                    <Button className='bg-[#1ABFAB] text-white dark:text-gray-900   block' type='button' onClick={() => {
+                    }}>
+                        <HiDownload className='mr-2 text-xl ' />
+                        <span>
+                            Share & Embed
+                        </span>
+                    </Button>
+                    <button className=' text-gray-900 dark:text-white flex items-center' type='button' onClick={() => {
+                    }}>
+                        <HiOutlinePlusCircle className='mr-2 text-xl ' />
+                        <span>
+                            Add a New Spreadsheet
+                        </span>
+                    </button>
+                </div>
+
+            </div>
+
+            <div className='my-[12px] flex items-center gap-[16px]'>
+                <img src={rectangleStackImg} className='w-[32px] h-[32px] ' />
+                <p className='text-2xl font-semibold text-gray-900 dark:text-white'>
+                    {workspace?.name}
+                </p>
+            </div>
+
+            <div className='py-[24px] gap-[24px] flex flex-row flex-wrap'>
+                <SpreadsheetCard time={workspace?.updatedAt} />
+            </div>
+
+
+        </MainContainer>
+    )
+}
+
+export default Spreadsheets

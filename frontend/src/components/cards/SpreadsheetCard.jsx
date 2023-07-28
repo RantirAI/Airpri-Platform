71000ed038e263316855182038152f79f15c1370
Folareen <sakawahab03@gmail.com>
2023-07-28 15:03:43 +0100

    refactor

--- frontend/src/components/cards/SpreadsheetCard.jsx
+++ frontend/src/components/cards/SpreadsheetCard.jsx
@@ -9,35 +9,6 @@ import { MdOpenInNew } from 'react-icons/md'
 import { getDateAndTime } from '../../utils/formatDate'
 import { useNavigate } from 'react-router-dom'
 
-// const SpreadsheetCard = ({ id, time, name }) => {
-//     const navigate = useNavigate()
-//     return (
-//         <div className='rounded-[8px] p-[24px] bg-white dark:bg-gray-800 shadow-md max-w-[350px]'>
-//             <div>
-//                 <img src={spreadsheetImg} />
-//             </div>
-//             <div className='my-[9px] text-gray-500 dark:text-gray-200'>
-//                 <LuSheet />
-//             </div>
-//             <p className='leading-tight text-base lg:text-lg font-bold capitalize mb-[9px] text-gray-900 dark:text-white'>
-//                 {name}
-//             </p>
-//             <p className='text-[10px] text-[xs] text-gray-500 dark:text-gray-200 font-normal mb-[12px]'>
-//                 {getDateAndTime(time)}
-//             </p>
-
-//             <button className='leading-tight text-xs lg:text-[14px] font-normal flex flex-row items-center text-[#1ABFAB] my-[9px]' onClick={() => {
-//                 navigate(`/spreadsheet/${id}`)
-//             }}>
-//                 <span>
-//                     Open and Edit
-//                 </span>
-//                 <MdOpenInNew className='ml-2 ' />
-//             </button>
-
-//         </div>
-//     )
-// }
 
 const SpreadsheetCard = ({ id, time, name }) => {
     const navigate = useNavigate()

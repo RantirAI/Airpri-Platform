a6c647b2abe3d7cac399bd770b26651a5b6d8e7b
Folareen <sakawahab03@gmail.com>
2023-08-01 18:49:02 +0100

    increased payload limit

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,8 @@ const authenticate = require('./src/middlewares/authenticate')
 const app = express()
 
 app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
 app.use('/api/v1', authRouter)
 app.use(authenticate)

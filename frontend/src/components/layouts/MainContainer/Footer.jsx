d0ca376a4bf743f8508ad3d9f0f4aa83af73a2f1
Folareen <sakawahab03@gmail.com>
2023-08-03 00:58:10 +0100

    fixed scroll to footer issue

--- frontend/src/components/layouts/MainContainer/Footer.jsx
+++ frontend/src/components/layouts/MainContainer/Footer.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FaFacebookF, FaGithub, FaGlobe, FaTwitter } from 'react-icons/fa'
+import { useLocation } from 'react-router-dom'
 
 const Footer = () => {
+
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+
   return (
     <footer className='w-full mt-auto py-[48px] px-[12px] bg-white dark:bg-[#111928] rounded-[8px] '>
       <div className='flex justify-between items-center text-gray-500 dark:text-white'>

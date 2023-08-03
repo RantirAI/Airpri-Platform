f417293ebb135b2fc51cda45b526d9affcb7253c
Folareen <sakawahab03@gmail.com>
2023-08-03 18:47:48 +0100

    added home link to not found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div>
+      <h2>
+        NotFound
+      </h2>
+      <Link to="/" className='text-blue-600 underline underline-offset-1'>Home</Link>
+    </div>
   )
 }
 

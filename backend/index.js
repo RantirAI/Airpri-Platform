cc82d9202212fd397e97021f5cb073b8b3e9ee3b
Folareen <sakawahab03@gmail.com>
2023-08-08 15:19:55 +0100

    try..

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(cors())
 app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }))
-app.timeout = 1000000
+app.timeout = 10000000
 
 app.use('/api/v1', authRouter)
 app.use('/api/v1/share', shareRouter)

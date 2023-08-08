c66cb98fc3d2d6677a87a0b017ab992bd43faba8
Folareen <sakawahab03@gmail.com>
2023-08-08 14:46:07 +0100

    increased limit

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ const authenticate = require('./src/middlewares/authenticate')
 const app = express()
 
 app.use(cors())
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }))
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }))
 
 app.use('/api/v1', authRouter)
 app.use('/api/v1/share', shareRouter)
